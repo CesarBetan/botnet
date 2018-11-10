e7f731a57077dbeb4cae8190ac7daa3ee1322672
allanloji <n16h7hun73r@gmail.com>
2018-11-10 02:53:19 -0600

    check failed connections and ports

--- analizer.py
+++ analizer.py
@@ -16,24 +16,51 @@ def detectBadPorts(rec):
     nuevoDF = nuevoDF[nuevoDF['destination_Port'] == True]
     dataframe = rec.ix[nuevoDF['index1']]
     counted_dataframe = {}
+    indices=[]
     for index, values in dataframe.iterrows():
         if values['destination_IP'] in counted_dataframe:
             counted_dataframe[values['destination_IP']] += 1;
+            if counted_dataframe[values['destination_IP']]>1:
+                indices.append(index)
         else:
             counted_dataframe[values['destination_IP']] = 1;
+            
     
     for key, values in counted_dataframe.items():
         #Change value to number of desire repeats for botnet
         if values > 1 :
             print(str(key) + " Es botnet")
+    print(rec.ix[indices])
     #print(counted_dataframe)
 
 
 
 
 def detectErrors(rec):
-    coincide = rec['destination_MAC'].isin(['00000000000'])
+    print('verga')
+    coincide = rec['destination_MAC'].isin(['000000000000'])
+    nuevoDF = coincide.to_frame()
+    nuevoDF['index1']= nuevoDF.index
+    nuevoDF = nuevoDF[nuevoDF['destination_MAC'] == True]
+    dataframe = rec.ix[nuevoDF['index1']]
+    counted_dataframe={}
+    indices=[]
+    for index, values in dataframe.iterrows():
+        if values['destination_MAC'] in counted_dataframe:
+            counted_dataframe[values['destination_MAC']] += 1;
+            if counted_dataframe[values['destination_MAC']]>1:
+                indices.append(index)
+        else:
+            counted_dataframe[values['destination_MAC']] = 1;
+            
+    for key, values in counted_dataframe.items():
+        #Change value to number of desire repeats for botnet
+        if values > 1 :
+            print(str(key) + " Es botnet")
+    print(rec.ix[indices])
 
-data = [['197.1.2.3','6667'], ['192.10.2.10','5364'], ['192.37.1.20','25'], ['192.123.45.1','1080'], ['192.234.53.1','27384'], ['192.34.5.7','13894'],['192.123.45.1','1080']]
-test = pd.DataFrame(data,columns=['destination_IP','destination_Port'])
-detectBadPorts(test)
+
+
+data = [['197.1.2.3','000000000000'], ['192.10.2.10','pacu189fab68'], ['192.37.1.20','asdfgh765432'], ['192.123.45.1','000000000000'], ['192.234.53.1','plmoknijb875'], ['192.34.5.7','000000000000'],['192.123.45.1','1ef9hb76gko9']]
+test = pd.DataFrame(data,columns=['destination_IP','destination_MAC'])
+detectErrors(test)
